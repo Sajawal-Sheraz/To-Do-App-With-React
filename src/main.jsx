5a85b54577faf5dc30ada7aa3bb337b1036536a4
Sajawal Sheraz <hello.sajawal@gmail.com>
2024-01-13 23:40:58 +0500

    save the day

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,18 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
