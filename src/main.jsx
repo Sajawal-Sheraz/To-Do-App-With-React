6c78df69aacaba2c6956661330d653edab2071ff
Sajawal Sheraz <hello.sajawal@gmail.com>
2024-01-20 10:31:46 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,8 +11,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
 );
