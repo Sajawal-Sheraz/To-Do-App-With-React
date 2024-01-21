8da3dc21ca7448a3ee0f3f6a8e3d3aadcfc113c6
Sajawal Sheraz <hello.sajawal@gmail.com>
2024-01-21 23:46:36 +0500

    some change

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
