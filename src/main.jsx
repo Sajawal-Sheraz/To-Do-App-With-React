6fba8174a2e237d1fa1dd79e8e6830633d5891e9
Sajawal Sheraz <hello.sajawal@gmail.com>
2024-01-23 23:54:14 +0500

    dummy commit

--- src/main.jsx
+++ src/main.jsx
@@ -17,13 +17,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
